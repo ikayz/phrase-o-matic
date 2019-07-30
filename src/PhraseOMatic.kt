a3f2d861aed53883d5a453360fe742619450492f
Isaac Miti <ikayz@live.com>
2019-07-30 22:12:55 +0200

    Add first three word arrays

---
+++ src/PhraseOMatic.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val wordArray1 = arrayOf("24/7", "multi-tier", "B-to-B", "Dynamic", "pervasive")
+
+    val wordArray2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
+
+    val wordArray3 = arrayOf("process", "paradigm", "solution", "portal", "vision")
+
+
+}
