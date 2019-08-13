d3f5e07153e89d0bcba5d9008ad9013c34e07089
Isaac Miti <ikayz@live.com>
2019-08-13 13:00:56 +0200

    Add array size checker

--- src/PhraseOMatic.kt
+++ src/PhraseOMatic.kt
@@ -1,8 +1,12 @@
 fun main(args: Array<String>) {
     val wordArray1 = arrayOf("24/7", "multi-tier", "B-to-B", "Dynamic", "pervasive")
-
     val wordArray2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
-
     val wordArray3 = arrayOf("process", "paradigm", "solution", "portal", "vision")
+
+    val arraySize1 = wordArray1.size
+    val arraySize2 = wordArray2.size
+    val arraySize3 = wordArray3.size
+
+
 
 }
